f58d9eb1469223d3c65174ba7b64daaf8d3c5aca
Borja Rafols <borjarafols@gmail.com>
2016-07-12 11:53:33 +0000

    Added postsController

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,8 +11,6 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        //ApplicationConfiguration config = (MyAppConfig) ctx.getBean("");
     }
 
 }
--- src/main/java/hello/Controllers/PostController.java
+++ src/main/java/hello/Controllers/PostController.java
@@ -1,7 +1,9 @@
 package hello.Controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;;
 
 @RestController
 public class PostController {
@@ -9,6 +11,16 @@ public class PostController {
     @RequestMapping("/posts")
     public String index() {
         return "List of posts";
+    };
+    
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public String get(@PathVariable("id") int postId){
+        return "get post";
+    } 
+    
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.POST)
+    public String post(@PathVariable("id") int postId){
+        return "post post";
     }
     
 }
